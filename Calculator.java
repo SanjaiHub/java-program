66c11663688ef0106e0d8993b1e702d70b16b5bd
Sanjay kumar <160604614+SanjaiHub@users.noreply.github.com>
2024-03-29 23:28:02 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args) {
+     /*   int a=12;
+        int b=2;
+        int add=a+b;
+        System.out.println(add);
+        int diff=a-b;
+        System.out.println(diff);
+        int mult=a*b;
+        System.out.println(mult);
+        int div=a/b;
+        System.out.println(div);
+     */
+  
+    }
+    
+}
